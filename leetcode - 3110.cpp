30b5bf715e3329b4fcf1538faffd70bb0c9eb0a9
Dharmik2005-Godalka <dharmikgodalka@gmail.com>
2026-08-11 09:41:29 +0530

    Leetcode Particular this problem no. 3110 solution.

---
+++ leetcode - 3110.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum = 0;
+
+        for(int i=0; i<s.size()-1; i++)
+        {
+            int diff = s[i] - s[i+1];
+            sum = sum + abs(diff);
+        }
+        return sum;
+    }
+};
