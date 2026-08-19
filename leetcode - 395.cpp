6ec19bff2e395456b95c093edaff1f1d7588ce65
Dharmik2005-Godalka <dharmikgodalka@gmail.com>
2026-08-19 12:39:19 +0530

    Updated Leetcode problem no: 395 solution...

--- leetcode - 395.cpp
+++ leetcode - 395.cpp
@@ -6,21 +6,18 @@ public:
         for(int i=0; i<s.size(); i++)
         {
             vector<int> count(26,0);
-
             for(int j =i;j<s.size(); j++)
             {
                 count[s[j] - 'a']++;
 
                 bool value = true;
-
-                for(int n =0;n<26; n++)
+                for(int n =0; n<26; n++)
                 {
                     if(count[n]>0 && count[n]<k)
                     {
                         value =false;
                     }
                 }
-
                 if(value)
                 {
                     result = max(result, j-i+1);
