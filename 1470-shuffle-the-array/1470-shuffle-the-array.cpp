88570c8dd3deb64bbe5e502c2da69fc565ff8573
Dharmik2005-Godalka <dharmikgodalka@gmail.com>
2026-08-28 20:48:56 +0530

    Time: 4 ms (65.71%), Space: 13.3 MB (97.48%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -6,7 +6,7 @@ public:
         for(int i = 0; i<n; i++)
         {
             result.push_back(nums[i]);
-            result.push_back(nums[i +n]);
+            result.push_back(nums[i+n]);
         }
         return result;
     }
