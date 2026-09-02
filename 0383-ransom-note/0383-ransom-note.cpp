4a81ef5aebb01e5a48ed9633914e6ac5637fdcc6
Dharmik2005-Godalka <dharmikgodalka@gmail.com>
2026-09-02 08:12:55 +0530

    Time: 0 ms (100%), Space: 11.4 MB (99.62%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count[26] = {};
+
+        for(int i=0; i<magazine.size(); i++)
+        {
+            count[magazine[i] - 'a']++;
+        }
+
+        for(int j=0; j<ransomNote.size(); j++)
+        {
+            count[ransomNote[j] - 'a']--;
+
+            if(count[ransomNote[j] - 'a']<0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
