434ec8782db4d501660ecf5d80fa09caac013b8b
Dharmik2005-Godalka <dharmikgodalka@gmail.com>
2026-09-22 09:38:28 +0530

    Time: 6 ms (5.48%), Space: 31.2 MB (20.74%) - LeetHub

---
+++ 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximizeExpressionOfThree(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int c = nums[0];
+        int a = nums[nums.size()-1];
+        int b = nums[nums.size()-2];
+
+        int d = a+b-c;
+        return d;
+    }
+};
